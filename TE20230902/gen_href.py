aebac0d5360477467eced14b8a45d547e4fb74da
XianyanXie <xx0294432@gmail.com>
2023-09-05 09:57:41 +0000

    美化css

--- TE20230902/gen_href.py
+++ TE20230902/gen_href.py
@@ -1,9 +1,11 @@
 import os
 
 def gen_href():
-    for file in os.listdir('.'):
+    filelist = os.listdir()
+    filelist.sort()
+    for file in filelist:
         if file.endswith('.html'):
-            print('<li><a href="https://stmoonar.github.io/'+file+'">'+file+'</a></li>')
+            print('\t\t<li><a href="https://stmoonar.github.io/TE20230902/'+file+'">'+file.replace('-', ' ').replace('.html', ' ')+'</a></li>')
 
 if __name__ == '__main__':
     gen_href()
